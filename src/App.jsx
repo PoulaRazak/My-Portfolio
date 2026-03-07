88cdb708ac5183e861f69ccfc16199f727e513ce
Poula Razak <drpolarazak@gmail.com>
2026-03-07 04:13:56 +0200

    change file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import Home from './components/Home/home.jsx'
-import Navbar from './components/Navbar/navbar.jsx'
+import Home from './components/Home/index.jsx'
+import Navbar from './components/Navbar/index.jsx'
 import SkillsSection from './components/Skills_section/section.jsx'
-import Services from './components/Services/services.jsx'
-import Footer from './components/Footer/footer.jsx'
+import Services from './components/MyServices/index.jsx'
+import Footer from './components/Footer/index.jsx'
 import Projects from './components/Projects/index.jsx'
 
 function App() {
