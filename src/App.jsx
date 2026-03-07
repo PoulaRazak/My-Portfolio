b8afaea1e86639d8a6baaabfff166d29e61d782d
Poula Razak <drpolarazak@gmail.com>
2026-03-07 04:45:02 +0200

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Home from './components/Home/index.jsx'
 import Navbar from './components/Navbar/index.jsx'
-import SkillsSection from './components/Skills_section/section.jsx'
+import SkillsSection from './components/MySkills/index.jsx'
 import Services from './components/MyServices/index.jsx'
 import Footer from './components/Footer/index.jsx'
 import Projects from './components/Projects/index.jsx'
